594fd5ca37fe78d6b07824a3c5ec4403da662e5e
Andreas Spannagel <spannagel.andreas@gmail.com>
2018-10-28 16:35:25 +0100

    Add basic styling with scss variables for colors and spacing. Add styling to button.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -92,24 +92,26 @@ export default class IndecisionApp extends React.Component {
 
     // Pass data and handle functions to child components
     return (
-      <div>
+      <div className="main">
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal
-          selectedOption={this.state.selectedOption}
-          handleClearSelectedOption={this.handleClearSelectedOption}
-        />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+          <OptionModal
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
+          />
+        </div>
       </div>
     )
   }
